881921952c517ad6cc2b018e887c643453a41c43
Carlos Hernandez <carloshparedes@gmail.com>
2024-07-02 19:28:18 +0200

    chore: Update main.py with Pydantic model configuration for Movie class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
@@ -9,11 +9,26 @@ app.version = "0.0.1"
 
 class Movie(BaseModel):
     id: Optional[int] = None
-    title: str
-    overview: str
-    year: str
+    title: str = Field(min_length=5, max_length=15)
+    overview: str = Field(min_length=10, max_length=50)
+    year: int = Field(le = 2022)
     rating: float 
     category: str
+
+    model_config = {
+            "json_schema_extra": {
+                "examples": [
+                {
+                    'id': 1,
+                    'title' : 'Crepusculo',
+                    'overview' : 'The twilight is almost better than sunday',
+                    'year' : '2022',
+                    'rating' : 9.5,
+                    'category' : 'Phantasy'
+                }
+                ]
+            }
+        }
 
 movies = [
     {
