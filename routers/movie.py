39b1cdd81d5ef57bbcfecbda8696ced4486f385b
Carlos Hernandez <carloshparedes@gmail.com>
2024-07-23 13:01:18 +0200

    Refactor get_movies_by_category function to use MovieService for movie retrieval

--- routers/movie.py
+++ routers/movie.py
@@ -53,7 +53,7 @@ def get_movie(id: int = Path(ge = 1, le=2000)) -> Movie:
 @movie_router.get('/movies/', tags=["Movies"], response_model=List[Movie])
 def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
     db = Session()
-    result = db.query(MovieModel).filter(MovieModel.category == category).all()
+    result = MovieService(db).get_movies_by_category(category)
     if not result:
         return JSONResponse(status_code=404, content={"message": "Movies not found"})
     return JSONResponse(content=jsonable_encoder(result))
